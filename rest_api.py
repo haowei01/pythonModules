03cf43261c56e0f4e530d97f535017e94a76242c
hunryzh2003 <hunryzh2003@github.com>
2014-11-19 23:57:32 -0800

    use urllib

--- rest_api.py
+++ rest_api.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # http://isbullsh.it/2012/06/Rest-api-in-python/
 
+# https://developer.yahoo.com/python/python-rest.html
+
 # use urllib2
 github_url = 'https://api.github.com/user/repos'
 
