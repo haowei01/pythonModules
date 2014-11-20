dda3f527e8e471ff047cc72656ebaa2ada8b02e7
hunryzh2003 <hunryzh2003@github.com>
2014-11-19 22:37:06 -0800

    compute delta

--- client.py
+++ client.py
@@ -9,11 +9,15 @@ port = 12345
 
 s.connect((host, port))
 s.send('PING\r')
+pre_time = time.time()
 recv_line = s.recv(1024)
 print recv_line
 matchObj = re.match(r'.* (\d+\.?\d*)', recv_line)
 if matchObj:
   print matchObj.group(1)
+  server_time = matchObj.group(1)
+  delta_time = float(server_time) - pre_time
+  print 'delta time ' + str(delta_time)
 else:
   print 'not matched'
 s.close
